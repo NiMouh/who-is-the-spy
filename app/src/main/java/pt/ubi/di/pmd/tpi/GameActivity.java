1be24455882e9209015f40edc89beb36ec028fae
Nimouh <simaoaugusto11@hotmail.com>
2022-10-25 21:07:37 +0100

    Ficheiros necessários

---
+++ app/src/main/java/pt/ubi/di/pmd/tpi/GameActivity.java
@@ -0,0 +1,15 @@
+package pt.ubi.di.pmd.tpi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
