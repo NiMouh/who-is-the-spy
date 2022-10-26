1ba151efdf3022534f9b99d5515e898f3195ceb9
Nimouh <simaoaugusto11@hotmail.com>
2022-10-26 19:53:22 +0100

    quase quase feito

--- app/src/main/java/pt/ubi/di/pmd/tpi/GameActivity.java
+++ app/src/main/java/pt/ubi/di/pmd/tpi/GameActivity.java
@@ -1,24 +1,62 @@
 package pt.ubi.di.pmd.tpi;
 
+import static pt.ubi.di.pmd.tpi.MainActivity.players;
+
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class GameActivity extends AppCompatActivity {
+
+    // Declaration of variables
+    Button next_player;
+    Button guess_btn;
+
+    // Declare an ArrayList with the remaining players
+    private ArrayList<Player> remainingPlayers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Initialize the ArrayList
+        remainingPlayers = new ArrayList<>(players);
+
+        // Initialize the buttons
+        next_player = findViewById(R.id.next_player);
+        guess_btn = findViewById(R.id.guess_btn);
+
+        // Declare an ArrayList with the round players and put them all the players in it
+        ArrayList<Player> roundPlayers = new ArrayList<>(players);
+
+        // The game starts
+        // It will choose the first player randomly in the roundPlayers ArrayList
+        // And remove him from the ArrayList
+        // If the next button is pressed, it will choose the next player randomly in the roundPlayers ArrayList and remove him from the ArrayList roundPlayers
+        // If the roundPlayers ArrayList is empty, it will show a reunion room
+        next_player.setOnClickListener(v -> {
+            // If the roundPlayers ArrayList is empty, it will show a reunion room
+            if (roundPlayers.isEmpty()) {
+                // Show the reunion room
+            } else {
+                // Show his name on the screen
+                // Choose the next player randomly in the roundPlayers ArrayList
+                // And remove him from the ArrayList
+            }
+        });
+
+        // If the guess button is clicked then it will show the options of the locations for the player to guess
+        guess_btn.setOnClickListener(v -> {
+            // Show the options of the locations for the player to guess
+            // If the player guesses correctly, the game ends and it will show the result
+            // If the player guesses incorrectly, it will expel him from the game
+        });
+
+
     }
-    // Começa o jogo.
-    // Escolhe um utilizador aleatório
-    // Acaba de ser feita a pergunta e resposta, aparece botão 'next'
-    // Segue este processo até acabar a ronda
-    // No final da ronda todos podem fazer uma votação (maioria vence)
-    // Fazer isto enquanto existir espiões ou enquanto tiver 3 ou mais restantes
-    // OBS.: Qualquer espião pode fazer a qualquer altura um palpite que está num botão 'palpite'
-    // aparece as opções de lugares, Ele escolhe um, caso esteja certo o jogo acaba e os espiões ganham
-    // caso contrário ele é expulso.
 
 }
--- app/src/main/java/pt/ubi/di/pmd/tpi/ResultActivity.java
+++ app/src/main/java/pt/ubi/di/pmd/tpi/ResultActivity.java
@@ -1,13 +1,28 @@
 package pt.ubi.di.pmd.tpi;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
+
+    // Declaration of variables
+    TextView win_role;
+    TextView win_names;
+    Button new_game;
+    Button exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        // Initialization of variables
+        win_role = findViewById(R.id.role_result);
+        win_names = findViewById(R.id.players_result);
+        new_game = findViewById(R.id.new_game_btn);
+        exit = findViewById(R.id.home_btn);
     }
 }
